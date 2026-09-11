a67316d229260939aa35031981347997a9981092
Côte Ouest Ads <dev@coteouest.ci>
2026-09-11 20:32:45 +0000

    Fix compatibilite R2 (checksum SDK)

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -21,6 +21,8 @@ export function getR2Client() {
     region: "auto",
     endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
     credentials: { accessKeyId, secretAccessKey },
+    requestChecksumCalculation: "WHEN_REQUIRED",
+    responseChecksumValidation: "WHEN_REQUIRED",
   });
 }
 
